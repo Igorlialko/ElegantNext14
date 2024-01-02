fffc4f4789bb70dbc7d1be2488ca476635f68bf5
Igor Lialko <igor.lialko@abz.agency>
2024-01-02 11:10:01 +0200

    refactor: icons in adventages block home page

--- src/app/_components/Advantages/Advantages.tsx
+++ src/app/_components/Advantages/Advantages.tsx
@@ -1,27 +1,24 @@
 import s from './advantages.module.scss';
+import FastDeliveryIcon from '@/app/_icons/FastDeliveryIcon';
 
 const cardMas = [
   {
-    url: '/images/fastDelivery.png',
-    alt: 'fastDelivery',
+    icon: <FastDeliveryIcon />,
     title: 'Free Shipping',
     text: 'Order above $200',
   },
   {
-    url: '/images/money.png',
-    alt: 'money',
+    icon: <FastDeliveryIcon />,
     title: 'Money-back',
     text: '30 days guarantee',
   },
   {
-    url: '/images/lock%2001.png',
-    alt: 'lock',
+    icon: <FastDeliveryIcon />,
     title: 'Secure Payments',
     text: 'Secured by Stripe',
   },
   {
-    url: '/images/call.png',
-    alt: 'call',
+    icon: <FastDeliveryIcon />,
     title: '24/7 Support',
     text: 'Phone and Email support',
   },
@@ -31,10 +28,8 @@ const Advantages = () => (
   <section className='_container'>
     <div className={s.cards}>
       {cardMas.map((card) => (
-        <div key={card.url} className={s.cardNumber}>
-          <div className={s.cardIcons}>
-            <img src={card.url} alt={card.alt} width={48} height={48} />
-          </div>
+        <div key={card.title} className={s.cardNumber}>
+          <div className={s.cardIcons}>{card.icon}</div>
           <div className={s.cardTitle}>{card.title}</div>
           <div className={s.cardText}>{card.text}</div>
         </div>
