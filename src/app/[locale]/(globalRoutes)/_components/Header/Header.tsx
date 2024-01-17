9519484bd28b84e106fdb344be6455887948fc2f
igorlialko <igorlialko8@gmail.com>
2024-01-17 12:58:45 +0200

    refactor: cart page

--- src/app/[locale]/(globalRoutes)/_components/Header/Header.tsx
+++ src/app/[locale]/(globalRoutes)/_components/Header/Header.tsx
@@ -4,7 +4,8 @@ import s from './header.module.scss';
 import SearchIcon from '@/app/_icons/SearchIcon';
 import UserActivity from '@/app/[locale]/(globalRoutes)/_components/Header/UserActivity/UserActivity';
 import MenuLinks from '@/app/[locale]/(globalRoutes)/_components/MenuLinks/MenuLinks';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+// import { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { useHeaderShadow } from '@/app/[locale]/(globalRoutes)/_components/Header/useHeaderShadow';
@@ -15,40 +16,41 @@ export default function Header() {
   const isShadow = useHeaderShadow();
 
   const [isAuthorized, setIsAuthorized] = useState(true);
-  const [isRed, setIsRed] = useState(false);
+  // const [isRed, setIsRed] = useState(false);
 
-  useEffect(() => {
-    const promiseTimeout = () =>
-      new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve(true);
-        }, 1000);
-        reject('Error for reject');
+  // useEffect(() => {
+  //   const promiseTimeout = () =>
+  //     new Promise((resolve: (b: boolean) => void, reject) => {
+  //       setTimeout(() => {
+  //         resolve(true);
+  //       }, 1000);
+  //
+  //       // fetch('http://localhost:3000/en/shop')
+  //     });
 
-        // fetch('http://localhost:3000/en/shop')
-      });
-
-    promiseTimeout()
-      .then((response) => {
-        console.log('response', response);
-      })
-      .catch((reason) => {
-        console.log('reason', reason);
-      })
-      .finally(() => {
-        console.log('finally');
-      });
-    console.log('mount');
-  }, []);
+  // promiseTimeout()
+  //   .then((response) => {
+  //     setIsRed(response);
+  //     console.log('response', response);
+  //   })
+  //   .catch((reason) => {
+  //     console.log('reason', reason);
+  //   })
+  //   .finally(() => {
+  //     console.log('finally');
+  //   });
+  //
+  //   console.log('mount');
+  // }, []);
 
   return (
     <header
       className={clsx(s.header, {
         [s.shadow]: isShadow,
       })}
-      style={{
-        backgroundColor: isRed ? 'red' : undefined,
-      }}
+      // style={{
+      //   backgroundColor: isRed ? 'red' : undefined,
+      // }}
     >
       <div className='_container'>
         <div className={s.headerMenu}>
